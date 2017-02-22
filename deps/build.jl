b4a0522b38da75718bb486e3148a35cb303266c6
Julian P <jul1278@gmail.com>
2017-02-22 16:55:13 +1000

    modified build.jl for windows

--- deps/build.jl
+++ deps/build.jl
@@ -2,8 +2,19 @@ using BinDeps
 
 @BinDeps.setup
 
-link = "https://github.com/peterwittek/somoclu/releases/download/1.7.2/somoclu-1.7.3-pre.tar.gz"
-libsomoclu = library_dependency("libsomoclu", aliases=["libsomoclu", "libsomoclu.so"], os=:Unix)
-provides(Sources, Dict(URI(link) => libsomoclu))
-provides(BuildProcess, Autotools(libtarget = joinpath("src", "libsomoclu.so"), configure_options=[AbstractString("--without-mpi")]), libsomoclu, os = :Unix)
-@BinDeps.install Dict(:libsomoclu => :libsomoclu)
+if is_windows()
+
+    link = "https://github.com/peterwittek/somoclu/releases/download/1.7.2/somoclu-1.7.3-pre.tar.gz"
+    libsomoclu = library_dependency("libsomoclu", aliases=["libsomoclu", "libsomoclu.dll"], os=:Windows)
+    provides(Sources, Dict(URI(link) => libsomoclu))
+    provides(BuildProcess, Autotools(libtarget = joinpath("src", "libsomoclu.dll"), configure_options=[AbstractString("--without-mpi")]), libsomoclu, os = :Windows)
+    @BinDeps.install Dict(:libsomoclu => :libsomoclu)
+    
+else
+
+    link = "https://github.com/peterwittek/somoclu/releases/download/1.7.2/somoclu-1.7.3-pre.tar.gz"
+    libsomoclu = library_dependency("libsomoclu", aliases=["libsomoclu", "libsomoclu.so"], os=:Unix)
+    provides(Sources, Dict(URI(link) => libsomoclu))
+    provides(BuildProcess, Autotools(libtarget = joinpath("src", "libsomoclu.so"), configure_options=[AbstractString("--without-mpi")]), libsomoclu, os = :Unix)
+    @BinDeps.install Dict(:libsomoclu => :libsomoclu)
+end
